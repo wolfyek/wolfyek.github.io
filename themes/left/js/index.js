b01548bac1bc7a45474596e7504af1dc2b70a873
wolfyek <93786414+wolfyek@users.noreply.github.com>
2024-01-02 05:44:19 +0100

    Add files via upload

---
+++ themes/left/js/index.js
@@ -0,0 +1,4 @@
+var PixiesetTheme=(function(my,$){function initCoverPhoto(){var $collectionCoverEl=$('#collection-cover'),previewWarningOffset=0;if($('#preview-warning').length>0)
+previewWarningOffset=$('#preview-warning').height();if($collectionCoverEl.length>0){if($('#nav-wrapper-mobile').is(':visible')){$collectionCoverEl.css('height',$(window).height()-previewWarningOffset);}else{$collectionCoverEl.css('height',$(window).height()-previewWarningOffset);$(window).resize(function(){$collectionCoverEl.css('height',$(window).height()-previewWarningOffset);});}
+$('#collection-cover .cover-arrow a').on('click',function(e){e.preventDefault();PixiesetHelpers.scrollPastCover();});}}
+my.init=function(){initCoverPhoto();};return my;}(window.PixiesetTheme||{},jQuery));
